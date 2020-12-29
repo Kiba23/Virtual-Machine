3b5353b522b31e4b8739a3411d35bde9e55453b4
Anton Kibalka <anton.kibalka@gmail.com>
2020-12-29 11:01:34 +0200

    Add project files.

---
+++ Virtual Machine/Commands.cs
@@ -0,0 +1,158 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Virtual_Machine
+{
+    public class Commands
+    {
+        string encr; // mb do through char[]
+
+        public Commands() // Reading the ecnryption file
+        {
+            FileInfo encryptionFilePath = new FileInfo("D:\\C# Training\\Labs\\Virtual Machine\\Virtual Machine\\bin\\Debug\\q1_encr.txt");
+            if (encryptionFilePath.Exists)
+            {
+                using (StreamReader sr = encryptionFilePath.OpenText())
+                {
+                    encr = sr.ReadToEnd();
+                }
+            }
+        }
+
+        // figure out how to modify not the copy of reg but the reg itself
+        public void INC(Program program)
+        {
+            int rightIndex = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+
+            program.reg[rightIndex]++;
+        }
+
+        public void DEC(Program program)
+        {
+            int rightIndex = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+
+            program.reg[rightIndex]--;
+        }
+
+        public void MOV(Program program)
+        {
+            int rightIndex = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+            int rightNumber = int.Parse(program.parameters[program.j].ElementAt(0).ToString());
+
+            program.reg[rightIndex] = rightNumber;
+        }
+
+        public void MOVC(Program program)
+        {
+            int rightNumber = int.Parse(program.parameters[program.j], System.Globalization.NumberStyles.HexNumber);
+
+            program.reg[0] = rightNumber;
+        }
+
+        public void LSL(Program program)
+        {
+            int rightIndex = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+
+            program.reg[rightIndex] = program.reg[rightIndex] << 1;
+        }
+
+        public void LSR(Program program)
+        {
+            int rightIndex = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+
+            program.reg[rightIndex] = program.reg[rightIndex] >> 1;
+        }
+
+        public void JMP(Program program) // all jump commands works wrong bcs of index (counter) wrong assigning
+        {
+            program.j = int.Parse(program.parameters[program.j], System.Globalization.NumberStyles.HexNumber);
+        }
+
+        public void JZ(Program program) // this shouldn't work bcs of the loop
+        {
+            if (program.flag)
+            {
+                program.j = int.Parse(program.parameters[program.j], System.Globalization.NumberStyles.HexNumber);
+            }
+        }
+
+        public void JNZ(Program program)
+        {
+            if (!program.flag)
+            {
+                program.j = int.Parse(program.parameters[program.j], System.Globalization.NumberStyles.HexNumber);
+            }
+        }
+
+        public void JFE(Program program) // check if it's really changing the 'j' inside the loop
+        {
+            if (program.fileEnd)
+            {
+                program.j = int.Parse(program.parameters[program.j], System.Globalization.NumberStyles.HexNumber);
+            }
+        }
+
+        public void RET(Program program)
+        {
+            program.flag = true;
+        }
+
+        public void ADD(Program program)
+        {
+            int Rx = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+            int Ry = int.Parse(program.parameters[program.j].ElementAt(0).ToString());
+
+            program.reg[Rx] = program.reg[Rx] + program.reg[Ry];
+        }
+
+        public void SUB(Program program)
+        {
+            int Rx = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+            int Ry = int.Parse(program.parameters[program.j].ElementAt(0).ToString());
+
+            program.reg[Rx] = program.reg[Rx] - program.reg[Ry];
+        }
+
+        public void XOR(Program program)
+        {
+            int Rx = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+            int Ry = int.Parse(program.parameters[program.j].ElementAt(0).ToString());
+
+            program.reg[Rx] = program.reg[Rx] ^ program.reg[Ry];
+        }
+
+        public void OR(Program program)
+        {
+            int Rx = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+            int Ry = int.Parse(program.parameters[program.j].ElementAt(0).ToString());
+
+            program.reg[Rx] = program.reg[Rx] | program.reg[Ry];
+        }
+
+        public void IN(Program program)
+        {
+            int Rx = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+
+            program.reg[Rx] = (int)encr.ElementAt(0);
+            encr = encr.Remove(0, 1);
+
+            if (encr.Length < 1)
+            {
+                program.fileEnd = true;
+            }
+
+            // some kind of operation with flag here
+        }
+
+        public void OUT(Program program)
+        {
+            int Rx = int.Parse(program.parameters[program.j].ElementAt(1).ToString());
+
+            using (StreamWriter sw = new StreamWriter("D:\\C# Training\\Labs\\Virtual Machine\\Virtual Machine\\bin\\Debug\\output.txt", false))
+            {
+                sw.Write(program.reg[Rx]);
+                // ascii converted
+            }
+        }
+    }
+}
---
+++ Virtual Machine/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Virtual_Machine
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.ReadFromFiles();
+
+            program.Formate();
+
+            Console.ReadLine();
+        }
+
+        public string[] commands;
+        public string[] parameters;
+        public int[] reg = new int[16]; // was - char[] reg = new char[16];
+        //public char[] memory = new char[256];
+        public bool flag = false;
+        public bool fileEnd = false;
+        public byte[] decr;
+        public int j;
+        StringBuilder decryptionString = new StringBuilder();
+        Commands cmd = new Commands();
+
+        private void ReadFromFiles()
+        {
+            FileInfo encryptionFilePath = new FileInfo("D:\\C# Training\\Labs\\Virtual Machine\\Virtual Machine\\bin\\Debug\\q1_encr.txt");
+            string decryptionFilePath = "D:\\C# Training\\Labs\\Virtual Machine\\Virtual Machine\\bin\\Debug\\decryptor.bin";
+
+            if (encryptionFilePath.Exists)
+            {
+                decr = File.ReadAllBytes(decryptionFilePath);
+                foreach (byte b in decr)
+                {
+                    decryptionString.Append(Convert.ToString(b, 16).PadLeft(2, '0'));
+                }
+            }
+        }
+
+        private void Formate()
+        {
+            int i = 0;
+
+            commands = new string[decryptionString.Length / 2 / 2];
+            parameters = new string[decryptionString.Length / 2 / 2];
+
+            while (decryptionString.Length > 1)
+            {
+                commands[i] = decryptionString.ToString().Substring(0, 2);
+                decryptionString.Remove(0, 2);
+
+                parameters[i] = decryptionString.ToString().Substring(0, 2);
+                decryptionString.Remove(0, 2);
+
+                i++;
+            }
+
+            Run();
+        }
+
+        private void Run()
+        {
+            // Creating the list of  all functions from Commands class
+            var commandList = new List<System.Reflection.MethodInfo>();
+            foreach (var func in typeof(Commands).GetMethods())
+            {
+                commandList.Add(func);
+            }
+
+            // Finding the needed function with commandsIndex array
+            int[] commandsIndex = new int[commands.Length];
+            for (j = 0; j < commands.Length; j++) // j - as a counter
+            {
+                if (!flag)
+                {
+                    commandsIndex[j] = Convert.ToInt32(commands[j], 16);
+
+                    commandList[commandsIndex[j] - 1].Invoke(cmd, new object[] { this }); // be careful with commandList array, it has spare methods
+                    //commandList[17 - 1].Invoke(cmd, new object[] { this });
+                }
+                else
+                {
+                    Console.WriteLine("Program Ends!\n");
+                }
+            }
+        }
+    }
+}
